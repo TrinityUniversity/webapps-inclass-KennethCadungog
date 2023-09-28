94333fd4c43ef95a5cde60fa31b7a1492c637d07
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2023-09-28 11:02:02 -0500

    Videos 1-6

---
+++ server/app/controllers/TaskList1.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+import play.api.i18n._
+
+
+@Singleton
+class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+    
+    def login = Action {
+        Ok(views.html.login1())
+    }
+
+    def validateLoginGet(username: String, password: String) = Action {
+        Ok("$username logged in with $password.")
+    }
+
+    def taskList = Action {
+        val tasks = List("task1","task2","task3","sleep","eat")
+        Ok(views.html.taskList1(tasks))
+    } 
+}
