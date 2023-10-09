2d790225a71307b158af1106da43e831893a14e8
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2023-10-09 11:10:22 -0500

    Vid 11-16 form value not found, old error too

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -4,12 +4,19 @@ import javax.inject._
 
 import play.api.mvc._
 import play.api.i18n._
+import play.api.data
+import play.api.data.Forms._
 
+case class LoginData(username: String, password: String)
 
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-    
-    def login = Action {
+    val loginForm = Form(mapping(
+        "Username" -> text(3, 10), 
+        "Password" -> text(8)
+        )(LoginData.apply)(LoginData.unapply))
+
+    def login = Action { implicit request =>
         Ok(views.html.login1())
     }
 
@@ -17,7 +24,7 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
         Ok(s"$username logged in with $password.")
     }
 
-    def validateLoginPost = Action { request => 
+    def validateLoginPost = Action { implicit request => 
         val postVals = request.body.asFormUrlEncoded
         postVals.map { args =>
           val username = args("username").head 
@@ -25,12 +32,16 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
           if(TaskListInMemoryModel.validateUser(username,password)) {
             Redirect(routes.TaskList1.taskList()).withSession("username" -> username)
           } else {
-            Redirect(routes.TaskList1.login())
+            Redirect(routes.TaskList1.login()).flashing("error" -> "Invalid username/password combination.")
           }
         }.getOrElse(Redirect(routes.TaskList1.login()))
     }
 
-    def createUser = Action { request => 
+    def validateLoginForm = Action { implicit request =>
+      Ok("")
+    }
+
+    def createUser = Action { implicit request => 
         val postVals = request.body.asFormUrlEncoded
         postVals.map { args =>
           val username = args("username").head 
@@ -38,12 +49,12 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
           if(TaskListInMemoryModel.createUser(username,password)) {
             Redirect(routes.TaskList1.taskList()).withSession("username" -> username)
           } else {
-            Redirect(routes.TaskList1.login())
+            Redirect(routes.TaskList1.login()).flashing("error" -> "User creation failed.")
           }
         }.getOrElse(Redirect(routes.TaskList1.login()))
     }
 
-    def taskList = Action { request =>
+    def taskList = Action { implicit request =>
         val usernameOption = request.session.get("username")
         usernameOption.map { username =>
             val tasks = TaskListInMemoryModel.getTask(username)
@@ -54,4 +65,29 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     def logout = Action {
         Redirect(routes.TaskList1.login()).withNewSession 
     }
+
+    def addTask = Action { implicit request =>
+      val usernameOption = request.session.get("username")
+      usernameOption.map { username =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map { args =>
+          val task = args("newTask").head
+          TaskListInMemoryModel.addTask(username, task);
+          Redirect(routes.TaskList1.taskList())
+        }.getOrElse(Redirect(routes.TaskList1.taskList()))
+      }.getOrElse(Redirect(routes.TaskList1.login())) 
+    }
+
+    def deleteTask = Action { implicit request =>
+      val usernameOption = request.session.get("username")
+      usernameOption.map { username =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map { args =>
+          val task = args("index").head.toInt
+          TaskListInMemoryModel.removeTask(username, index);
+          Redirect(routes.TaskList1.taskList())
+        }.getOrElse(Redirect(routes.TaskList1.taskList()))
+      }.getOrElse(Redirect(routes.TaskList1.login())) 
+    } 
+
 }
--- server/app/controllers/TaskListInMemoryModel.scala
+++ server/app/controllers/TaskListInMemoryModel.scala
@@ -21,7 +21,13 @@ object TaskListInMemoryModel {
         tasks.get(username).getOrElse(Nil)
     }
 
-    def addTask(username: String, task: String): Unit = ???
+    def addTask(username: String, task: String): Unit = {
+        tasks(username) = task :: tasks.get(username).getOrElse(Nil)
+    }
 
-    def removeTask(username: String, index: Int): Boolean = ???
+    def removeTask(username: String, index: Int): Boolean = {
+        if (index < 0 || tasks.get(username).isEmpty || index >= tasks(username.length)) false
+        tasks(username) = tasks(username).patch(index, Nil, 1)
+        true 
+    }
 }
