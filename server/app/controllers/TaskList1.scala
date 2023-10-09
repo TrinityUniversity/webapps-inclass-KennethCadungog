bd887869ab3fc76f9d9cfaa26bd9fc6dbec4d115
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2023-10-09 13:45:30 -0500

    Vid 17 Er: TaskList1.scala:33

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -4,20 +4,20 @@ import javax.inject._
 
 import play.api.mvc._
 import play.api.i18n._
-import play.api.data
+import play.api.data._ 
 import play.api.data.Forms._
 
 case class LoginData(username: String, password: String)
 
 @Singleton
-class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class TaskList1 @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
     val loginForm = Form(mapping(
         "Username" -> text(3, 10), 
         "Password" -> text(8)
         )(LoginData.apply)(LoginData.unapply))
 
     def login = Action { implicit request =>
-        Ok(views.html.login1())
+        Ok(views.html.login1(loginForm))
     }
 
     def validateLoginGet(username: String, password: String) = Action {
@@ -38,7 +38,15 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     }
 
     def validateLoginForm = Action { implicit request =>
-      Ok("")
+      loginForm.bindFromRequest.fold(
+          formWithError => BadRequest(views.html.login1(formWithErrors)),
+          ld => 
+            if(TaskListInMemoryModel.validateUser(ld.username,ld.password)) {
+            Redirect(routes.TaskList1.taskList()).withSession("username" -> ld.username)
+            } else {
+            Redirect(routes.TaskList1.login()).flashing("error" -> "Invalid username/password combination.")
+            }
+      )
     }
 
     def createUser = Action { implicit request => 
