338f38a059c186aeff8a32790b860034f968d7e1
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2023-10-20 14:13:07 -0500

    Put Lewis email code + comment some main.scala ln

--- server/test/TaskList1Spec.scala
+++ server/test/TaskList1Spec.scala
@@ -4,7 +4,6 @@ import org.scalatestplus.play.OneBrowserPerSuite
 import org.scalatestplus.play.HtmlUnitFactory
 
 class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
-    // Error, might be related to React files. Probably should delete React
     "Task List 1" must {
         "login and access functions" in {
             go to s"http://localhost:$port/login1"
@@ -14,16 +13,16 @@ class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowser
             click on "username-login"
             textField("username-login").value = "web"
             click on "password-login"
-            pwdField("password").value = "apps"
+            pwdField(id("password-login")).value = "apps"
             submit()
             eventually {
                 //Unsure if correct
                 println("In eventually")
                 println(currentUrl)
-                pageTitle mustBe "Login"
+                pageTitle mustBe "Message Board"
                 println("title passed")
                 find(cssSelector("h1")).isEmpty mustBe false 
-                println("h2 found")
+                println("h1 found")
                 find(cssSelector("h1")).foreach(e => e.text mustBe "@username's Message Board")
             }
         }
