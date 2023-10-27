d38be85d1369239a02626daa35f96ed713259075
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2023-10-27 12:27:26 -0500

    Vd 50; Websocket can't connect or open

--- server/app/controllers/WebSocketChat.scala
+++ server/app/controllers/WebSocketChat.scala
@@ -10,9 +10,14 @@ import akka.actor.Actor
 import play.api.libs.streams.ActorFlow
 import akka.actor.ActorSystem
 import akka.stream.Materializer
+import actors.ChatActor
+import akka.actor.Props
+import actors.ChatManager
 
 @Singleton 
 class WebSocketChat @Inject() (cc: ControllerComponents)(implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
+    val manager = system.actorOf(Props[ChatManager], "Manager")
+
     def index = Action { implicit request =>
         Ok(views.html.chatPage())
     }
@@ -20,7 +25,7 @@ class WebSocketChat @Inject() (cc: ControllerComponents)(implicit system: ActorS
     def socket = WebSocket.accept[String, String] { request =>
         println("Getting socket")
         ActorFlow.actorRef { out =>
-          ???
+          ChatActor.props(out, manager)
         }    
     }
 }
--- server/public/javascript/chat.js
+++ server/public/javascript/chat.js
@@ -8,3 +8,16 @@ const outputArea = document.getElementById("chat-area");
 const socketRoute = document.getElementById("ws-route").value;
 const socket = new WebSocket(socketRoute.replace("http","ws")); 
 
+inputField.onkeydown = (event) => {
+    if(event.key === 'Enter') {
+        socket.send(inputField.value);
+        input.value = '';
+    }
+}
+
+socket.onopen = (event) => socket.send("New user connected.");
+
+socket.onmessage = (event) => {
+    outputArea.value += '\n' + event.data;
+}
+
