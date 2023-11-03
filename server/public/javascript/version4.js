4f23e8036aa2daee4679a5d90fe952d8e5be3ab3
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2023-11-03 17:20:01 -0500

    Vid 55 Err: Create TaskList 3 Functions

--- server/public/javascript/version4.js
+++ server/public/javascript/version4.js
@@ -2,6 +2,45 @@ console.log("Running version 4.");
 
 const ce = React.createElement
 
+class LoginComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {loginName: "", loginPass: "", createName: "", cratePass: ""};
+    }
+    render() {
+        return ce('div', null,
+            ce('h2', null, 'Login:'),
+            ce('br'),
+            'Username: ',
+            ce('input', {type: "text", id: "loginName", value: this.state.loginName, onChange: e => this.changeHandler(e)}),
+            ce('br'),
+            'Password: ',
+            ce('input', {type: "password", id: "loginPass", value: this.state.loginPass, onChange: e => this.changeHandler(e)}),
+            ce('br'),
+            ce('button', {onclick: e => this.login(e)}, 'Login'),
+            ce('h2', null, 'Create User:'),
+            ce('br'),
+            'Username: ',
+            ce('input', {type: "text", id: "createName", value: this.state.createName, onChange: e => this.changeHandler(e)}),
+            ce('br'),
+            'Password: ',
+            ce('input', {type: "password", id: "createPass", value: this.state.createPass, onChange: e => this.changeHandler(e)}),
+            ce('br'),
+            ce('button', {onClick: e => this.createUser(e)}, 'Create User')     
+        );
+    }
+
+    changeHandler(e) {
+        console.log(e.target['id']);
+    }
+}
+
+ReactDOM.render(
+    ce(LoginComponent, null, null),
+    document.getElementById('react-root')
+);
+
+/*
 function StatelessHello(props) {
     return ce('div', null, `Hello ${props.toWhat}`);
 }
@@ -20,7 +59,26 @@ class Hello extends React.Component {
     }
 }
 
+class SimpleForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {textInput: ""}
+    }
+    render() {
+        return ce('input', {type: "text", value: this.state.textInput, onChange: (e) => this.changeHandler(e)});
+    }
+
+    changeHandler(e) {
+        this.setState({textInput: event.target.value});
+    }
+}
+
 ReactDOM.render(
-    ce('div', null, ce(Hello, {toWhat: 'World'}, null), ce(StatelessHello, {toWhat: 'Earth'}, null)), 
+    ce('div', null, 
+        ce(Hello, {toWhat: 'World'}, null), 
+        ce(StatelessHello, {toWhat: 'Earth'}, null), 
+        ce(SimpleForm, null, null)
+    ),
     document.getElementById('react-root')
 );
+*/
