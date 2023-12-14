4aa17b4ab0e12b5113ba6430778d6d61c19191c5
2022-02-01 <kcadungo@pandora03.cs.trinity.edu>
2023-12-13 18:28:23 -0600

    Task 9 In Progress. Changed tables

--- server/app/models/TaskListDatabaseModel.scala
+++ server/app/models/TaskListDatabaseModel.scala
@@ -1,49 +1,49 @@
-// package models
+package models
 
-// import slick.jdbc.PostgresProfile.api._ 
-// import scala.concurrent.ExecutionContext
-// import models.Tables._ 
-// import scala.concurrent.Future 
-// import org.mindrot.jbcrypt.BCrypt
+import slick.jdbc.PostgresProfile.api._ 
+import scala.concurrent.ExecutionContext
+import models.Tables._ 
+import scala.concurrent.Future 
+import org.mindrot.jbcrypt.BCrypt
+import shared.MessageItem
 
-// class TaskListDatabaseModel(db: Database)(implicit ec: ExecutionContext) {
-//     def validateUser(username: String, password: String): Future[Option[Int]] = {
-//         val matches = db.run(Users.filter(userRow => userRow.username === username).result)
-//         matches.map(userRows => userRows.headOption.flatMap {
-//             userRow => if (BCrypt.checkpw(password, userRow.password)) Some(userRow.id) else None
-//         })
-//     }
+class TaskListDatabaseModel(db: Database)(implicit ec: ExecutionContext) {
+    def validateUser(username: String, password: String): Future[Option[Int]] = {
+        val matches = db.run(Users.filter(userRow => userRow.username === username).result)
+        matches.map(userRows => userRows.headOption.flatMap {
+            userRow => if (BCrypt.checkpw(password, userRow.password)) Some(userRow.id) else None
+        })
+    }
 
-//     def createUser(username: String, password: String): Future[Option[Int]] = {
-//         val matches = db.run(Users.filter(userRow => userRow.username === username).result)
-//         matches.flatMap { userRows =>
-//             if (userRows.isEmpty) {
-//             db.run(Users += UsersRow(-1, username, BCrypt.hashpw(password, BCrypt.gensalt())))
-//                 .flatMap{ addCount => 
-//                     if (addCount > 0) db.run(Users.filter(userRow => userRow.username === username).result)
-//                         .map(_.headOption.map(_.id))
-//                     else Future.successful(None)
-//                 }
-//             } else Future.successful(None)
-//         }
-//     }
+    def createUser(username: String, password: String): Future[Option[Int]] = {
+        val matches = db.run(Users.filter(userRow => userRow.username === username).result)
+        matches.flatMap { userRows =>
+            if (userRows.isEmpty) {
+            db.run(Users += UsersRow(-1, username, BCrypt.hashpw(password, BCrypt.gensalt())))
+                .flatMap{ addCount => 
+                    if (addCount > 0) db.run(Users.filter(userRow => userRow.username === username).result)
+                        .map(_.headOption.map(_.id))
+                    else Future.successful(None)
+                }
+            } else Future.successful(None)
+        }
+    }
 
-//     def getTasks(username: String): Future[Seq[TaskItem]] = {
-//         db.run(
-//             (for {
-//                 user <- Users if user.username === username
-//                 item <- Items if item.userId === user.id
-//             } yield {
-//                 item
-//             }).result 
-//         ).map(items => items.map(item => TaskItem(item.itemId, item.text.getOrElse(""))))
-//     }
 
-//     def addTask(userid: Int, task: String): Future[Int] = {
-//         db.run(Items += ItemsRow(-1, Option(userid), Option(task)))
-//     }
+    def getMessage(username: String): Future[Seq[MessageItem]] = {
+        db.run(
+            (for {
+                user <- Users if user.username === username
+                message <- Messages if messages.receiver === user.username || messages.username === "evryone"
+            } yield {
+                message
+            }).result 
+        ).map(messages => messages.map(message => MessageItem(message.sender, message.text)))
+    }
 
-//     def removeTask(itemId: Int): Future[Boolean] = {
-//         db.run( Items.filter(_.itemId === itemId).delete ).map(count => count > 0)
-//     }
-// }
+
+    def addMessage(receiver: Int, message: String): Future[Int] = {
+        db.run(Messages += MessagesRow(-1, Option(receiver), Option(message)))
+    }
+
+}
